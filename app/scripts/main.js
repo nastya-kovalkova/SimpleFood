ed8000dffe100144c0d42421c46cd71f6b16afe5
nastya-kovalkova <nastyakovalkova333@gmail.com>
2024-05-14 03:58:24 +0300

    section-popular, add mixitup, fixed links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,15 @@
 $(function () {
   $(window).on("scroll", function () {
     const scrollValue = window.scrollY;
-    console.log(scrollValue);
 
-    //   if (scrollValue > 0) {
-    //     $(".header__inner").addClass("header__inner--mini");
-    //   } else $(".header__inner").removeClass("header__inner--mini");
-    // });
-
-    // $(".header__inner").toggleClass("header__inner--mini");
-
-    // $(function () {
-    //   $(window).trigger("scroll");
+    if (scrollValue > 0) {
+      $(".header__inner").addClass("header__inner--mini");
+    } else $(".header__inner").removeClass("header__inner--mini");
   });
+
+  $(function () {
+    $(window).trigger("scroll");
+  });
+
+  var mixer = mixitup(".popular__content");
 });
--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,11 @@ function styles() {
 }
 
 function scripts() {
-  return src(["app/scripts/main.js", "node_modules/jquery/dist/jquery.js"])
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "app/scripts/mixitup.js",
+    "app/scripts/main.js",
+  ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/scripts"))
