f0699c7c705cee68057fe2bfc195abc544c4c5cb
nastya-kovalkova <nastyakovalkova333@gmail.com>
2024-05-27 02:25:20 +0300

    a lot of

--- gulpfile.js
+++ gulpfile.js
@@ -74,6 +74,11 @@ function scripts() {
     "node_modules/jquery/dist/jquery.js",
     "node_modules/mixitup/dist/mixitup.js",
     "node_modules/slick-carousel/slick/slick.js",
+    "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+    "node_modules/swiper/swiper.js",
+    "node_modules/swiper/swiper-bundle.js",
+    "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+    "node_modules/rateyo/src/jquery.rateyo.js",
     "app/scripts/main.js",
   ])
     .pipe(concat("main.min.js"))
